f7bf845f14fcff3d473a9205845aa302ec86a406
Yuristam <yuristamasanakunov@gmail.com>
2022-11-12 16:41:02 +0600

    added repositories and interfaces folders

--- JustInTime.BLL/DomainModels/Note.cs
+++ JustInTime.BLL/DomainModels/Note.cs
@@ -1,4 +1,6 @@
 ﻿
+using JustInTime.BLL.Enums;
+
 namespace JustInTime.BLL.DomainModels
 {
     public class Note
@@ -8,5 +10,6 @@ namespace JustInTime.BLL.DomainModels
         public string Description { get; set; }
         public string ColorHex { get; set; } // for the color of notes (in the future in Frontend)
         public DateTime DateCreated { get; set; } // the day and time when the note was created (it will create date automatically)
+        public NotesType NotesType { get; set; }
     }
 }
