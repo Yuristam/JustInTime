b13a887c715189d7872fef7b48f714fb45f585c2
Yuristam <yuristamasanakunov@gmail.com>
2022-11-18 16:03:11 +0600

    fixed edit problem in Note

--- JustInTime.WebApp/Controllers/NoteController.cs
+++ JustInTime.WebApp/Controllers/NoteController.cs
@@ -83,7 +83,7 @@ namespace JustInTime.WebApp.Controllers
         // POST: Animal/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPut]
+        [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Title,Description,ColorHex,DateCreated,NotesType,Id")] Note note)
         {
