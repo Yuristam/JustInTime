ad267f3c9ff81df711d532f48c4ee24cffbdc522
Yuristam <yuristamasanakunov@gmail.com>
2022-11-18 23:09:17 +0600

    added frontend folder myPhotos( added background photo(attack on titan via internet and spirited away (local in folder)))

--- JustInTime.DAL/Database/Contexts/NotesDbContext.cs
+++ JustInTime.DAL/Database/Contexts/NotesDbContext.cs
@@ -14,9 +14,11 @@ namespace JustInTime.DAL.Database.Contexts
         {
             
         }
-
-        public DbSet<Note> Notes { get; set; } // this is just ordinary note
-        public DbSet<CheckList> CheckLists { get; set; } // this is the List of notes
         
+        public DbSet<Note> Notes { get; set; } // this is just ordinary note
+     /*   public DbSet<CheckList> CheckLists { get; set; } // this is the List of notes*/
+
+        // i need to write onModelCreaeting here with has key or has no key (and then add migration)
+
     }
 }
