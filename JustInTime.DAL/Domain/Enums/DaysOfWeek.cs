d16c9188dabbf3ac308cebce8037190f59a12565
Yuristam <yuristamasanakunov@gmail.com>
2022-11-26 17:03:27 +0600

    added sleepTracker but didn't added a full realisation

---
+++ JustInTime.DAL/Domain/Enums/DaysOfWeek.cs
@@ -0,0 +1,8 @@
+﻿namespace JustInTime.DAL.Domain.Enums
+{
+    public enum DaysOfWeek
+    {
+        Monday, Tuesday, Wednesday, Thursday,
+        Friday, Saturday, Sunday
+    }
+}
---
+++ JustInTime.WebApp/Controllers/SleepTracker.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JustInTime.WebApp.Controllers
+{
+    public class SleepTracker : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
