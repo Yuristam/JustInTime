6fdc6c69384f5629a1dc9205f5b8cc7cc82268b8
Yuristam <yuristamasanakunov@gmail.com>
2022-11-29 11:25:27 +0600

    changed some thing

--- JustInTime.DAL/Database/Contexts/NotesDbContext.cs
+++ JustInTime.DAL/Database/Contexts/NotesDbContext.cs
@@ -17,6 +17,8 @@ namespace JustInTime.DAL.Database.Contexts
         }
         
         public DbSet<Note> Notes { get; set; } // this is just ordinary note
+        public DbSet<NoteType> NoteTypes { get; set; } // this is for many-to-many 
+        public DbSet<ColorHex> ColorHexes { get; set; } // this is for many-to-many 
         public DbSet<ToDo> ToDos { get; set; } // this is ToDo notes
         /*   public DbSet<CheckList> CheckLists { get; set; } // this is the List of notes*/
 
